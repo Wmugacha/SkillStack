00d086ea911d64316ab2d0060091568f4f1895fc
Wmugacha <wilfredmugacha@gmail.com>
2024-04-24 14:28:08 +0300

    dashboard routes

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -4,12 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-    <p className="text-3xl font-medium text-sky-700">
-      Hello World
-    </p>
-    <Button variant="destructive">
-      Click me
-    </Button>
+  This is a protected page
     </div>
   );
 }
--- app/(group)/wilfred/page.tsx
+++
@@ -1,9 +0,0 @@
-const WilfredPage = () => {
-    return ( 
-        <div>
-            Wilfred's Portfolio
-        </div>
-     );
-}
- 
-export default WilfredPage;
