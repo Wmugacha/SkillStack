3325b9db8ec131ed4ddf87633d1e2228c1a3ff1c
Wmugacha <wilfredmugacha@gmail.com>
2024-05-11 07:47:17 +0300

    middleware error fixed

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,12 @@ import {
 } from '@clerk/nextjs/server';
 
 const isProtectedRoute = createRouteMatcher([
-  '/dashboard(.*)'
+  '/'
 ]);
 
 export default clerkMiddleware((auth, req) => {
   if (isProtectedRoute(req)) auth().protect();
-});
+}, { debug: true });
 
 export const config = {
   matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
